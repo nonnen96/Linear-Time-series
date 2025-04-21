b5c196cdfa4f09f252e2c9679d0f19ef0d75ab55
nathsdm <nathan.soares_de_melo@telecom-sudparis.eu>
2025-04-21 14:36:51 +0200

    fixed R script

--- script.R
+++ script.R
@@ -3,21 +3,17 @@ library(forecast)
 library(tseries)
 library(ggplot2)
 
-# Load the dataset
-df <- read.csv("valeurs_mensuelles.csv", sep=";", dec=",", stringsAsFactors=FALSE)
+# Load the dataset - the file has a complex structure with multiple columns
+df <- read.csv("valeurs_mensuelles.csv", sep=";", dec=".", stringsAsFactors=FALSE)
 
 # Preview data
 head(df)
 
-# Parse date and value
-df$Date <- as.Date(df$DATE, format="%Y-%m-%d")
-df$Value <- as.numeric(gsub(",", ".", df$VALEUR))
+# Convert the second column to numeric and create a time series object
+df[, 2] <- as.numeric(df[, 2])
+time_series <- ts(df[, 2])
 
-# Create a time series object
-start_year <- as.numeric(format(min(df$Date), "%Y"))
-start_month <- as.numeric(format(min(df$Date), "%m"))
-ice_ts <- ts(df$Value, start=c(start_year, start_month), frequency=12)
-
-# Plot the raw time series
-autoplot(ice_ts) + ggtitle("Fabrication de glaces et sorbets (raw)") +
-  ylab("Indice de Production (base 100 en 2015)") + xlab("Année")
+# Plot the time series
+plot(time_series, type = "l", col = "blue", lwd = 2, 
+  xlab = "Time", ylab = "Values", 
+  main = "Evolution of Values in the Second Column")
